bfe72819ad3cce93c4258dda375dbad1a2da7790
Hiko <amir.amuri@yahoo.de>
2021-03-24 22:49:09 +0100

    ye

---
+++ Krakout/src/krakout/movement/nonPlayerObject/Item.java
@@ -0,0 +1,70 @@
+package krakout.movement.nonPlayerObject;
+
+import krakout.movement.Position;
+
+public class Item {
+    //Position of the item
+    public double x;
+    public double y;
+    //declares what kind of item it is
+    public int status;
+    //When being hit;
+    public boolean hit;
+    //Movement speed
+    public double speedInPixel;
+    //Ammount of items
+    public int ammount;
+    //Livepoints
+    public int live;
+    //Size of item
+    public double size;
+    //Color of item
+    public String color;
+    //Initating PowerUp movement
+    Position item = new Position(x, y);
+
+    //Constructor without an input
+    public Item() {
+        this(0, 0, 1, false, 2, 0, 0, 5, "green");
+    }
+
+    //Constructor
+    public Item(double x, double y, int status, boolean hit, double speedInPixel, int ammount, int live, double size, String color) {
+        this.x = x;
+        this.y = y;
+        this.status = status;
+        this.hit = hit;
+        this.speedInPixel = speedInPixel;
+        this.ammount = ammount;
+        this.live = live;
+        this.size = size;
+        this.color = color;
+    }
+
+    public String itemStatus(int status){
+        switch(status){
+            case 0:
+                return "PowerUp";
+            case 1:
+                return "Wall";
+            case 2:
+                return "Brick";
+            default:
+                return "none";
+
+        }
+    }
+
+    //adjusting it to the PixelSpeed
+    public void updatePosition() {
+        item.left(this.speedInPixel);
+        item.right(this.speedInPixel);
+        item.up(this.speedInPixel);
+        item.down(this.speedInPixel);
+    }
+
+    @Override
+    public String toString() {
+        return "Item, " + itemStatus(this.status) + " : " + this.item;
+    }
+}
---
+++ Krakout/src/krakout/movement/nonPlayerObject/Pinball.java
@@ -0,0 +1,66 @@
+package krakout.movement.nonPlayerObject;
+
+import krakout.movement.Position;
+
+public class Pinball {
+    //Position of the ball
+    public double x;
+    public double y;
+    //When hit destroy
+    public boolean hit;
+    //Ammount of balls
+    public int ammount;
+    //Movement Speed
+    public double speedInPixel;
+    //Size of ball
+    public double size;
+    //Color of ball
+    public String color;
+    //Initating pinball movement
+    Position pinball = new Position(x, y);
+
+    //Constructor without an input
+    public Pinball() {
+        this(0, 0, false, 0, 3, 5, "red");
+    }
+
+    //Constructor
+    public Pinball(double x, double y, boolean hit, int ammount, double speedInPixel, double size, String color) {
+        this.x = x;
+        this.y = y;
+        this.hit = hit;
+        this.ammount = ammount;
+        this.speedInPixel = speedInPixel;
+        this.size = size;
+        this.color = color;
+
+    }
+
+    //adjusting it to the PixelSpeed
+    public void updatePosition() {
+        pinball.left(this.speedInPixel);
+        pinball.right(this.speedInPixel);
+        pinball.up(this.speedInPixel);
+        pinball.down(this.speedInPixel);
+    }
+
+    /*
+        //Damaging Objects
+        public void damage(boolean hit){
+            if(hit=true){
+                bounce(hit);
+            }
+        }
+
+        //Bouncing of walls or Objects
+        public void bounce(boolean hit){
+            if(hit=true){
+            }
+    }
+
+     */
+    @Override
+    public String toString() {
+        return "Pinball: " + pinball;
+    }
+}
