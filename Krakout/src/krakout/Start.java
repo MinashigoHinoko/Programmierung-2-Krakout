36b1806e3b27e1986c790e2cf15924740e16e151
Hiko <amir.amuri@yahoo.de>
2021-04-07 22:13:05 +0200

    Update Start.java

--- Krakout/src/krakout/Start.java
+++ Krakout/src/krakout/Start.java
@@ -5,8 +5,8 @@ import krakout.gameview.GameLoopManager;
 public class Start {
 
     public static void main(String[] args) {
-        /**
-         * Initialising GameLoopManager to have a Game Screen
+        /*
+          Initialising GameLoopManager to have a Game Screen
          */
         GameLoopManager gameLoopManager = new GameLoopManager();
         gameLoopManager.startGame();
